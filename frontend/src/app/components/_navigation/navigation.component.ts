f5cc8d701b3a4fbb1dadf9849b0bedd7617b78d0
Marko Kostic <marko.k@codemancystudio.com>
2024-03-16 21:53:36 +0100

    clock

--- frontend/src/app/components/_navigation/navigation.component.ts
+++ frontend/src/app/components/_navigation/navigation.component.ts
@@ -1,27 +1,30 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { logoutUser } from '../../store/current-user/current.user.actions';
 import { Store } from '@ngrx/store';
 import { MatDrawer } from '@angular/material/sidenav';
 import { hasAnyElement } from '@shared/utils/common.utils';
-import { Observable } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 import { selectLoggedInUserRole } from '../../store/current-user/current.user.selectors';
 import { RolesEnum } from '@shared/enums/roles.enum';
 import { AppState } from '../../app.state';
 import { Router } from '@angular/router';
 import { routes } from '../../app-routing.module';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css'],
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
   @ViewChild('drawer') drawer!: MatDrawer;
   drawerOpened: boolean;
   public navItems: Array<{ title: string, path: string, icon: string, role: RolesEnum }>;
   currentRole: Observable<RolesEnum | null>;
   protected readonly hasAnyElement = hasAnyElement;
   protected readonly RolesEnum = RolesEnum;
+  currentTime: Date;
+  private timeSubscription!: Subscription;
 
   constructor(private _store: Store<AppState>, private _router: Router) {
     this.drawerOpened = true;
@@ -35,10 +38,19 @@ export class NavigationComponent {
           role: !!route.data!['role'] ? route.data!['role'] as RolesEnum : RolesEnum.User,
         }),
       );
+    this.currentTime = new Date();
   }
 
   onLogout() {
     this._store.dispatch(logoutUser());
+  }
+
+  ngOnInit(): void {
+    this.timeSubscription = interval(1000)
+      .pipe(map(() => new Date()))
+      .subscribe(time => {
+        this.currentTime = time;
+      });
   }
 
   handleDrawerToggle() {
